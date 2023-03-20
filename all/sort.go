56a4606c35f5a15fbbfa00b8ee89d1b56e8457ba
Haobo Wang <379723120@qq.com>
2023-03-20 22:21:42 +0800

    sort: 堆排序，归并排序

--- all/sort.go
+++ all/sort.go
@@ -4,6 +4,7 @@ import (
 	"math/rand"
 )
 
+// 快速排序
 func QuickSort(a []int) {
 	quickSort(a, 0, len(a)-1)
 }
@@ -29,4 +30,76 @@ func findPos(a []int, l, r int) int {
 	}
 	a[p], a[l] = a[l], a[p]
 	return p
+}
+
+// 堆排序
+func deepDown(a []int, n, i int) {
+	l, r := i<<1+1, i<<1+2
+	if l >= n {
+		return
+	}
+	if r >= n {
+		if a[i] < a[l] {
+			a[i], a[l] = a[l], a[i]
+			deepDown(a, n, l)
+		}
+		return
+	}
+	if a[i] < a[l] {
+		if a[l] < a[r] {
+			a[i], a[r] = a[r], a[i]
+			deepDown(a, n, r)
+		} else {
+			a[i], a[l] = a[l], a[i]
+			deepDown(a, n, l)
+		}
+	} else {
+		if a[i] < a[r] {
+			a[i], a[r] = a[r], a[i]
+			deepDown(a, n, r)
+		}
+	}
+}
+func buildHeap(a []int) {
+	n := len(a)
+	for i := n/2 - 1; i >= 0; i-- {
+		deepDown(a, n, i)
+	}
+}
+
+func heapSort(a []int) {
+	buildHeap(a)
+	n := len(a)
+	for n > 0 {
+		a[0], a[n-1] = a[n-1], a[0]
+		n--
+		deepDown(a, n, 0)
+	}
+}
+
+// 归并排序
+func mergeSort(a []int) []int {
+	n := len(a)
+	if n <= 1 {
+		return a
+	}
+	left := mergeSort(a[:n/2])
+	right := mergeSort(a[n/2:])
+	return mergeForSort(left, right)
+}
+func mergeForSort(a, b []int) []int {
+	m, n := len(a), len(b)
+	i, j, ans := 0, 0, []int(nil)
+	for i < m && j < n {
+		if a[i] < b[j] {
+			ans = append(ans, a[i])
+			i++
+		} else {
+			ans = append(ans, b[j])
+			j++
+		}
+	}
+	ans = append(ans, a[i:]...)
+	ans = append(ans, b[j:]...)
+	return ans
 }
