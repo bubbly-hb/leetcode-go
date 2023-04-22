5fad2cc726268eb14c80c3e2dc93f020697e8a61
hhhhhb <379723120@qq.com>
2023-04-22 12:51:00 +0800

    io: 读入一行未知个数的整数数组

--- all/io.go
+++ all/io.go
@@ -14,5 +14,12 @@ func bufferIO() {
 	var n int
 	fmt.Fscan(in, &n) // 如果行数未知，可以根据 Fscan 的第一个返回值是否为正来决定
 
+	// s, _ := in.ReadString('\n')  // 读入一行未知个数的整数数组
+	// ss := strings.Fields(s)
+	// a := make([]int, len(ss))
+	// for i, v := range ss {
+	// 	a[i], _ = strconv.Atoi(v)
+	// }
+
 	fmt.Fprintln(out, n)
 }
